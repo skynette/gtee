9646af2e26c40d70a0bf171b4b8c1a179335272c
joshua hassan <cutejosh2@gmail.com>
2025-01-09 19:53:40 +0100

    Refactor LucideReact imports and add new link in Footer component

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
 import { DynamicImage } from './dynamic-image';
+import { Pill } from 'lucide-react';
 
 export default function Logo({
     width = 100,
@@ -31,10 +32,15 @@ interface BrandProps {
 
 export function Brand({ className }: BrandProps) {
     return (
-        <Link href="/" className={className}>
+        <Link href="https://pump.fun" className={className}>
             <div className="flex items-center gap-2">
                 <Logo width={32} />
-                {/* <span className="text-x select-none font-bold">aswa</span> */}
+                {/* <Link
+                    href=""
+                    target="_blank"
+                    className="transition-colors hover:text-primary">
+                </Link> */}
+                <Pill className="h-5 w-5" />
             </div>
         </Link>
     );
